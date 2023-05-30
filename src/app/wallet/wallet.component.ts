7732c058c7c96f040289d8b744d3acd7a8aca82c
Bataitay <duongmg99@gmail.com>
2023-05-31 00:00:29 +0700

    optimized wallet

--- src/app/wallet/wallet.component.ts
+++ src/app/wallet/wallet.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 import { LoanService } from '../service/loan.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-wallet',
   templateUrl: './wallet.component.html',
-  styleUrls: ['./wallet.component.css']
+  styleUrls: ['./wallet.component.css'],
+
 })
 export class WalletComponent implements OnInit {
-  loan: any;
   token: any;
-  user: any;
+  loan$!: Observable<any>;
+  user$!: Observable<any>;
+  isDataLoaded: boolean = false;
+  private loanCache: { [key: string]: Observable<any> } = {};
+  private userCache: { [key: string]: Observable<any> } = {};
+
   constructor(private loanService: LoanService) { }
 
   ngOnInit(): void {
@@ -18,9 +24,16 @@ export class WalletComponent implements OnInit {
   }
 
   getMoneyLoan() {
-    this.loanService.getMoneyLoan(JSON.parse(this.token).id).subscribe(res => {
-      this.loan = res.sum || 0;
-      this.user = res.user || '';
-    });
+    const userId = JSON.parse(this.token).id;
+    if (this.loanCache[userId]) {
+      // Trả về kết quả đã lưu nếu có trong cache
+      return this.loanCache[userId];
+    } else {
+    this.loan$ = this.loanService.getMoneyLoan(userId);
+    this.user$ = this.loanService.getMoneyLoan(userId);
+    this.loanCache[userId] = this.loan$;
+    this.userCache[userId] = this.user$;
+    this.isDataLoaded = true;
+    }
   }
 }
