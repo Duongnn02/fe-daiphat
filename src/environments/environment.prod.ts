fba1785c9d2a5504af178f8db3c3af7986afcaec
Bataitay <duongmg99@gmail.com>
2023-06-02 19:53:14 +0700

    update id

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,9 @@
 export const environment = {
-  production: true
+  production: true,
+   url: 'http://admin.nndbe.site/api/',
+  urlImg: 'http://admin.nndbe.site/',
+  push : {
+    key: '95b1ad6835628d8b81be',
+    cluster: 'ap1'
+  }
 };
