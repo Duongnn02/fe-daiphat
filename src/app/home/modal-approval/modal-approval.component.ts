f70ca326599658c35e253aec5990f1453881643b
Bataitay <duongmg99@gmail.com>
2023-06-20 21:46:09 +0700

    udpate contract

--- src/app/home/modal-approval/modal-approval.component.ts
+++ src/app/home/modal-approval/modal-approval.component.ts
@@ -2,6 +2,7 @@ import {Component, Input, OnInit} from '@angular/core';
 import {NgbModal} from "@ng-bootstrap/ng-bootstrap";
 import {LoanService} from "../../service/loan.service";
 import { environment } from 'src/environments/environment';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-modal-approval',
@@ -12,13 +13,16 @@ export class ModalApprovalComponent implements OnInit {
   @Input() loanPackage: any;
   url: any;
   constructor(private modalService: NgbModal,
-  private loanService: LoanService) { }
+  private loanService: LoanService,
+  private router: Router,
+  ) { }
 
   ngOnInit(): void {
-    this.url = environment.urlImg;
   }
-  closeModal() {
+  closeModal(id: any) {
+    this.url = environment.urlImg + 'read-contract/'+id;
     this.modalService.dismissAll();
+    this.router.navigate([this.url])
     this.loanService.viewed().subscribe();
   }
 }
